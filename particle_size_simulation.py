42922f60d2cee05bcccd39986ba7168f51b4379c
t1020yy <yangytian1020@163.com>
2023-11-06 00:04:32 +0300

    Результаты расчетов были пропущены через Boxplot и error bar

--- particle_size_simulation.py
+++ particle_size_simulation.py
@@ -24,74 +24,81 @@ def process_images(img1, img2):
     
 
 if __name__ == "__main__":
-    modeling_number = np.arange(100)
+    modeling_number = np.arange(10)
     expose_time_starts = 0.050 - np.random.rand(modeling_number.shape[0]) * 0.100
-    
     parameters = []
     result = []
     result_params = []
-    cams_trans_vec_x = []
+    cams_trans_vec_x = [] 
 
-    for i in modeling_number:
-        param = ModelingParabolaParameters()
-        param.x_start_trajectory = -12 #mm
-        param.y_start_trajectory = 15 #mm
-        param.start_speed = 0.6 * 10**3 #mm/s
-        param.expose_time_start = expose_time_starts[i] 
-        param.expose_time = 0.100
-        # param.particle_diameter = 0.01 + 0.01 * (i // 10)
-        param.particle_diameter = 0.05
-        # param.start_angle = 40 + 0.8 * (i // 10)
-        # param.start_speed = (0.35 + 0.01 * (i // 10)) * 10**3 #mm/s
-        param.cams_trans_vec_x = 5.0 + 10 * random.random()
+    cams_trans_vec_x_values = np.linspace(5,15,30)
+    cams_rot_y_values = np.linspace(0,0.7,30)
+    for vec_x in cams_trans_vec_x_values:
+        for rot_ys in cams_rot_y_values:
+            for i in modeling_number:
+                param = ModelingParabolaParameters()
+                param.x_start_trajectory = -12 #mm
+                param.y_start_trajectory = 15 #mm
+                param.start_speed = 0.6 * 10**3 #mm/s
+                param.expose_time_start = expose_time_starts[i] 
+                param.expose_time = 0.100
+                # param.particle_diameter = 0.01 + 0.01 * (i // 10)
+                param.particle_diameter = 0.05
+                # param.start_angle = 40 + 0.8 * (i // 10)
+                # param.start_speed = (0.35 + 0.01 * (i // 10)) * 10**3 #mm/s
+                param.cams_trans_vec_x = vec_x
 
-        param.cams_rot_z = 0.01 + 20 * random.random()
+                param.cams_rot_y = rot_ys
 
-        parameters.append(param)
+                parameters.append(param)
 
 
     for param in parameters:
 
         img1, img2, _ = get_simulated_image(param)
         con_components1, con_components2, cur_component, matched_component = process_images(img1, img2)
+        if img1 is not None and img2 is not None:
+            key = display_processing_results(
+                img1,
+                img2,
+                con_components1,
+                con_components2,
+                cur_component, 
+                matched_component,
+                'Modeled image for cam #1',
+                'Modeled image for cam #2',
+                True,
+                wait_time=500
+            )
 
-        key = display_processing_results(
-            img1,
-            img2,
-            con_components1,
-            con_components2,
-            cur_component, 
-            matched_component,
-            'Modeled image for cam #1',
-            'Modeled image for cam #2',
-            True,
-            wait_time=500
-        )
+            cameraMatrix1 = param.cam1_K
+            distCoeffs1 = param.cam1_dist
+            cameraMatrix2 = param.cam2_K
+            distCoeffs2 = param.cam2_dist
+            R = param.cam2_R
+            T = param.cam2_T   
 
-        cameraMatrix1 = param.cam1_K
-        distCoeffs1 = param.cam1_dist
-        cameraMatrix2 = param.cam2_K
-        distCoeffs2 = param.cam2_dist
-        R = param.cam2_R
-        T = param.cam2_T   
+            P1 = cameraMatrix1 @ np.hstack((np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]), np.array([[0, 0, 0]]).T))
+            P2 = cameraMatrix2 @ np.hstack((R, np.array([T]).T))
 
-        P1 = cameraMatrix1 @ np.hstack((np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]), np.array([[0, 0, 0]]).T))
-        P2 = cameraMatrix2 @ np.hstack((R, np.array([T]).T))
+            if matched_component is not None:
+                particle = Particle_track(matched_component, cur_component, P1, P2)
+                particle.particle_radius = 100e-06
+                particle.particle_density = 2200
+                particle.voltage = 2500
+                particle.grid_height = param.y_start_trajectory
+                particle.image_name = "Modeled image"
 
-        if matched_component is not None:
-            particle = Particle_track(matched_component, cur_component, P1, P2)
-            particle.particle_radius = 100e-06
-            particle.particle_density = 2200
-            particle.voltage = 2500
-            particle.grid_height = param.y_start_trajectory
-            particle.image_name = "Modeled image"
-
-            result.append(particle)
-            result_params.append((particle.Alpha, particle.V0, particle.surface, particle.parabola, particle.parameters))
-            print(f'Сохранено {len(result)} треков')
+                result.append(particle)
+                result_params.append((particle.Alpha, particle.V0, particle.surface, particle.parabola, particle.parameters))
+                print(f'Сохранено {len(result)} треков')
+            else:
+                result.append(None)
+                print(f'Не удалось найти стереопару параболы')
         else:
             result.append(None)
-            print(f'Не удалось найти стереопару параболы')
+            print(f'Не удалось смоделировать')
+
                                         
     file_name = f'modeling_results_{datetime.datetime.now(): %Y-%m-%d_%H-%M-%S}.pickle'
     with open(file_name, 'wb') as f:
--- process_results.py
+++ process_results.py
@@ -2,9 +2,11 @@ import pickle
 
 import numpy as np
 from matplotlib import pyplot as plt
+import statsmodels.api as sm
+from scipy.optimize import curve_fit
 
 
-FILE_NAME = 'modeling_results_ 2023-11-02_16-22-14.pickle'
+FILE_NAME = 'modeling_results_ 2023-11-04_00-44-47.pickle'
 
 with open(FILE_NAME, 'rb') as f:
     parameters, result = pickle.load(f)
@@ -23,53 +25,142 @@ for i in range(len(parameters)):
         parameter = parameters[i]
         particle = result[i]
 
-        angle_err.append(parameter.start_angle - np.abs(particle[0]))
-        velocity_err.append(parameter.start_speed * 10**-3 - particle[1])
-        # diameter.append(parameter.particle_diameter)
-        start_angle.append(parameter.start_angle)
+        angle_err.append(abs(parameter.start_angle - np.abs(particle[0])))
+        velocity_err.append(abs(parameter.start_speed * 10**-3 - particle[1]))
+        diameter.append(parameter.particle_diameter)
+        # start_angle.append(parameter.start_angle)
         # start_speed.append(parameter.start_speed)
-        cams_trans_vec_x_values.append(parameter.cams_trans_vec_x)
+        # cams_trans_vec_x_values.append(parameter.cams_trans_vec_x)
 
-        relative_rotation = np.dot(np.linalg.inv(parameter.cam1_R), parameter.cam2_R)
-
-        # 将相对旋转矩阵转换为欧拉角
-        relative_euler_angles = np.degrees(np.array([np.arctan2(relative_rotation[2, 1], relative_rotation[2, 2]),
-                                                    np.arctan2(-relative_rotation[2, 0], np.sqrt(relative_rotation[2, 1] ** 2 + relative_rotation[2, 2] ** 2)),
-                                                    np.arctan2(relative_rotation[1, 0], relative_rotation[0, 0])]))
-
-        # 计算夹角
-        angle_between_cameras = np.linalg.norm(relative_euler_angles)
+        relative_rotation =parameter.cam2_R
+        relative_rotation_y = np.degrees(np.arctan2(relative_rotation[2, 0], relative_rotation[0, 0])) 
+        angle_between_cameras = np.abs(relative_rotation_y)
         angle_between_cameras_values.append(angle_between_cameras)
 
+#计算移动距离，旋转向量与角度差或速度差的关系图
+# x = np.array(cams_trans_vec_x_values)
+# y = np.array(angle_between_cameras_values)
+# z = np.array(angle_err)
+# plt.subplot(121)
+# contour = plt.tricontourf(x, y, z, cmap='viridis')
+# cbar = plt.colorbar(contour)
+# cbar.ax.tick_params(labelsize=16) 
+# plt.xlabel('cams_trans_vec_x (mm)', fontsize = 20)
+# plt.ylabel('angle_between_cameras (°)', fontsize = 20)
+# plt.title('angle_err', fontsize = 20)
+# plt.xticks(fontsize = 20)  
+# plt.yticks(fontsize = 20)
+# plt.grid()
+# plt.subplot(122)
+# z = np.array(velocity_err)
+# contour = plt.tricontourf(x, y, z, cmap='viridis')
+# cbar = plt.colorbar(contour)
+# cbar.ax.tick_params(labelsize=16) 
+# plt.xlabel('cams_trans_vec_x (mm)', fontsize = 20)
+# plt.ylabel('angle_between_cameras(°)', fontsize = 20)
+# plt.title('velocity_err', fontsize = 20)
+# plt.xticks(fontsize = 20)  
+# plt.yticks(fontsize = 20)
+# plt.grid()
+# plt.show()
+
+diameter = np.array(diameter)
+# start_speed = np.array(start_speed)
+angle_err = np.array(angle_err)
+velocity_err = np.array(velocity_err)
+unique_diameters = np.unique(diameter)
+# unique_start_speed = np.unique(start_speed)
+# unique_start_angle = np.unique(start_angle)
+
+mean_angle_err = [np.mean(angle_err[diameter == d]) for d in unique_diameters]
+mean_velocity_err = [np.mean(velocity_err[diameter == d]) for d in unique_diameters]
+std_dev_angle_err = [np.std(angle_err[diameter == d]) for d in unique_diameters]
+std_dev_velocity_err = [np.std(velocity_err[diameter == d]) for d in unique_diameters]
 
 
+
+# #计算平均值和原始数据的error bar图
+# plt.subplot(121)
+# plt.errorbar(unique_start_angle, mean_angle_err, yerr=std_dev_angle_err, fmt='o', color='b', ecolor='r', capsize=5)
+# # plt.plot(diameter, angle_err, 'bo')
+# plt.xlabel('start_angle (°)', fontsize = 20)
+# plt.ylabel('Angle error (°)', fontsize = 20)
+# plt.xticks(fontsize = 20)  
+# plt.yticks(fontsize = 20)
+# plt.gca().xaxis.get_label().set_ha('center')  # 设置x轴标签居中对齐
+# plt.gca().yaxis.get_label().set_ha('center')
+# plt.grid()
+# plt.subplot(122)
+# plt.errorbar(unique_start_angle, mean_velocity_err, yerr=std_dev_velocity_err, fmt='o', color='b', ecolor='r', capsize=5)
+# # plt.plot(diameter, velocity_err, 'bo')
+# plt.xlabel('start_angle (°)', fontsize = 20)
+# plt.ylabel('Velocity error (m/s)', fontsize = 20)
+# plt.xticks(fontsize = 20)  
+# plt.yticks(fontsize = 20)
+# plt.gca().xaxis.get_label().set_ha('center')  # 设置x轴标签居中对齐
+# plt.gca().yaxis.get_label().set_ha('center')
+# plt.grid()
+# plt.show()
+
+
+# #拟合后的曲线值作为平均值
+coefficients = np.polyfit(unique_diameters, mean_angle_err, 2)
+coefficients1 = np.polyfit(unique_diameters, mean_velocity_err, 2)
+polynomial = np.poly1d(coefficients)
+polynomial1 = np.poly1d(coefficients1)
+# 生成拟合曲线上的点
+x_fit = np.linspace(min(unique_diameters), max(unique_diameters), 200)
+y_fit = polynomial(x_fit)
+new_mean_angle_err = polynomial(unique_diameters)
+
+y_fit1 = polynomial1(x_fit)
+new_mean_velocity_err = polynomial1(unique_diameters)
+# 绘制图像时使用新的 mean_angle_err
 plt.subplot(121)
-plt.plot(start_angle, angle_err, 'bo')
+plt.errorbar(unique_diameters, new_mean_angle_err, yerr=std_dev_angle_err, fmt='o', color='b', ecolor='r', capsize=5)
+plt.xlabel('Partice diameter (mm)', fontsize=20)
+plt.ylabel('Angle error (°)', fontsize=20)
+plt.xticks(fontsize=20)  
+plt.yticks(fontsize=20)
+plt.gca().xaxis.get_label().set_ha('center')
+plt.gca().yaxis.get_label().set_ha('center')
 plt.grid()
+
 plt.subplot(122)
-plt.plot(start_angle, velocity_err, 'bo')
+plt.errorbar(unique_diameters, new_mean_velocity_err, yerr=std_dev_velocity_err, fmt='o', color='b', ecolor='r', capsize=5)
+plt.xlabel('Partice diameter (mm)', fontsize=20)
+plt.ylabel('velocity_err (m/s)', fontsize=20)
+plt.xticks(fontsize=20)  
+plt.yticks(fontsize=20)
+plt.gca().xaxis.get_label().set_ha('center')
+plt.gca().yaxis.get_label().set_ha('center')
 plt.grid()
 plt.show()
 
-# plt.subplot(121)
-# plt.plot(angle_between_cameras_values, angle_err, 'bo')
-# plt.grid()
-# plt.subplot(122)
-# plt.plot(angle_between_cameras_values, velocity_err, 'bo')
-# plt.grid()
-# plt.show()
 
-# plt.subplot(121)
-# plt.scatter(cams_trans_vec_x_values, angle_between_cameras_values, c=angle_err, cmap='viridis')
-# plt.xlabel('param.cams_trans_vec_x')
-# plt.ylabel('angle_between_cameras')
-# plt.colorbar(label='angle_err')
-# plt.grid()
-# plt.subplot(122)
-# plt.scatter(cams_trans_vec_x_values, angle_between_cameras_values, c=velocity_err, cmap='viridis')
-# plt.xlabel('param.cams_trans_vec_x')
-# plt.ylabel('angle_between_cameras')
 
-# plt.colorbar(label='velocity_err')
-# plt.grid()
-# plt.show()
+
+# #计算Boxplot图
+data_a = [angle_err[diameter == d] for d in unique_diameters]
+data_v = [velocity_err[diameter == d] for d in unique_diameters]
+unique_diameters = np.around(unique_diameters, 2)
+# unique_start_speed = [int(d) for d in unique_start_speed]
+# unique_start_angle = [int(d) for d in unique_start_angle]
+plt.boxplot(data_a, labels=unique_diameters)
+plt.xlabel('Partice diameter (mm)', fontsize = 20)
+plt.ylabel('Angle error (°)', fontsize = 20)
+
+plt.xticks(rotation=45,fontsize = 15)   
+plt.yticks(fontsize = 15)
+plt.gca().xaxis.get_label().set_ha('center')
+plt.gca().yaxis.get_label().set_ha('center')
+plt.show()
+
+plt.boxplot(data_v, labels=unique_diameters)
+plt.xlabel('Partice diameter (mm)', fontsize = 20)
+plt.ylabel('Velocity error (m/s)', fontsize = 20)
+plt.xticks(rotation=45,fontsize = 15)  
+plt.yticks(fontsize = 15)
+plt.gca().xaxis.get_label().set_ha('center')  
+plt.gca().yaxis.get_label().set_ha('center')
+plt.show()
